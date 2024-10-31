eddd18f7f4a4544b15ca2460be9dbb9ca110c9fc
yaro360 <yaro360@gmail.com>
2024-10-31 15:13:32 -0700

    Fix Firebase Hosting configuration with Express server

--- firebase-project/index.js
+++ firebase-project/index.js
@@ -1,2 +1,18 @@
-console.log("Firebase App Hosting - Food Transparency Project");
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Handle root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
